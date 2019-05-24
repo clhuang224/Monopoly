78230e5179ce2ade69db89776b537f49af6b1910
chiaya710623 <chiaya710623@gmail.com>
2019-05-24 11:51:44 +0800

    Update House.h House.cpp     void setLevel(unsigned);     unsigned getPrice();     Designed Signs of Houses

--- Project2-Monopoly/House.cpp
+++ Project2-Monopoly/House.cpp
@@ -1,9 +1,39 @@
 #include "House.h"
 
-House::House(unsigned newPosition, unsigned newType, unsigned newLevel) : Block(newPosition,newType)
+House::House(unsigned newPosition, string newName, unsigned newType, unsigned newLevel)
+    : Block(newPosition, newName, newType)
 {
+    vector<string> sign;
+    switch (newLevel)
+    {
+    case 0:
+        sign = { "          ",
+                 "  ◢█◣  ",
+                 "    █    ",
+                 "          " };
+        break;
+    case 1:
+        sign = { "    ▲    ",
+                 "  ◢█◣  ",
+                 "    █    ",
+                 "          " };
+        break;
+    case 2:
+        sign = { "   ◢◣   ",
+                 " ◢██◣ ",
+                 "  ███  ",
+                 "  ███  " };
+        break;
+    case 3:
+        sign = { "  ◢█◣  ",
+                 "◢███◣",
+                 "  █★█  ",
+                 "  ███  " };
+        break;
+    }
+    setSign(sign);
     level = newLevel;
-    // price = 依據文件的level跟type找出對應的價錢
+    // priceList = 依據文件找出價錢
 }
 
 unsigned House::getLevel()
@@ -13,6 +43,39 @@ unsigned House::getLevel()
 
 void House::setLevel(unsigned newLevel)
 {
+    vector<string> newSign;
+    switch (newLevel)
+    {
+    case 0:
+        newSign = { "          ",
+                    "  ◢█◣  ",
+                    "    █    ",
+                    "          " };
+        break;
+    case 1:
+        newSign = { "    ▲    ",
+                    "  ◢█◣  ",
+                    "    █    ",
+                    "          " };
+        break;
+    case 2:
+        newSign = { "   ◢◣   ",
+                    " ◢██◣ ",
+                    "  ███  ",
+                    "  ███  " };
+        break;
+    case 3:
+        newSign = { "  ◢█◣  ",
+                    "◢███◣",
+                    "  █★█  ",
+                    "  ███  " };
+        break;
+    }
+    setSign(newSign);
     level = newLevel;
-    // price = 依據文件的level跟type找出對應的價錢
+}
+
+unsigned House::getPrice()
+{
+    return priceList[level];
 }
--- Project2-Monopoly/House.h
+++ Project2-Monopoly/House.h
@@ -1,15 +1,26 @@
-#pragma once
+#ifndef HOUSE_H
+#define HOUSE_H
+
 #include "Block.h"
 class House :
     public Block
 {
 public:
-    House(unsigned, unsigned, unsigned);
+    House(unsigned, string, unsigned, unsigned);
+
+    // 取得當下等級
     unsigned getLevel();
+
+    // 設定等級 sign也會隨之改變
     void setLevel(unsigned);
+
+    // 取得當下價格
+    unsigned getPrice();
 
 private:
     unsigned level;
-    unsigned price;
+    vector<unsigned> priceList;
 };
+
+#endif // !HOUSE_H
 
