5f4208eebfb0874cf99d6f9d3150a3bc0cd10602
Pines Bettur <44045861+PinesBettur@users.noreply.github.com>
2019-05-31 02:54:16 +0800

    小修正

--- Project2-Monopoly/Map.cpp
+++ Project2-Monopoly/Map.cpp
@@ -67,10 +67,22 @@ void Map::updateMap(array<int, 4> playerPositions)
 	}
 
 	//插入玩家位置
-	map[playerPositions[0]]->setOutput(1);
-	map[playerPositions[1]]->setOutput(2);
-	map[playerPositions[2]]->setOutput(3);
-	map[playerPositions[3]]->setOutput(4);
+	if (playerPositions[0] != -1)
+	{
+		map[playerPositions[0]]->setOutput(1);
+	}
+	if (playerPositions[1] != -1)
+	{
+		map[playerPositions[1]]->setOutput(2);
+	}
+	if (playerPositions[2] != -1)
+	{
+		map[playerPositions[2]]->setOutput(3);
+	}
+	if (playerPositions[3] != -1)
+	{
+		map[playerPositions[3]]->setOutput(4);
+	}
 
 	for (int i = 0; i <= width - 1; i++) //左方的 Block
 	{
